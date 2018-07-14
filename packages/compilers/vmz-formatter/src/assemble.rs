74f01ae198ae06315b1228b82d3ea79f829943ab
aster <aster@vers.site>
2018-07-14 18:27:19 +0000

    🔧 Lock `oxc` to git tag and add `vmz-formatter` crate
    Pin workspace oxc_* to crates_v0.144.0 (incl. oxc_formatter/css) and introduce an independent authoring formatter with `EditorConfig` + IR format APIs.

---
+++ packages/compilers/vmz-formatter/src/assemble.rs
@@ -0,0 +1,159 @@
+//! Faithful `.vmz` reassembly: preserve router/meta/lang/attrs/block order.
+
+use vmz_compiler::{DataBlock, ParsedVmz, ScriptBlock, ScriptLanguage, StyleLanguage};
+
+use crate::editorconfig::EditorSettings;
+
+/// Reassemble a formatted SFC from parsed structure + formatted bodies.
+pub fn assemble_vmz(
+    parsed: &ParsedVmz,
+    client: &str,
+    server: Option<&str>,
+    style: Option<&str>,
+    settings: &EditorSettings,
+) -> String {
+    let nl = settings.newline();
+    let mut parts: Vec<String> = Vec::new();
+
+    if let Some(router) = &parsed.router {
+        parts.push(emit_data_block("router", router, settings));
+    }
+    if let Some(meta) = &parsed.meta {
+        parts.push(emit_data_block("meta", meta, settings));
+    }
+
+    parts.push(emit_tagged_block(
+        "template",
+        None,
+        &indent_block(&parsed.template.content, settings),
+        settings,
+    ));
+
+    if let (Some(style_body), Some(style_block)) = (style, parsed.style.as_ref()) {
+        let lang_attr = match style_block.lang {
+            StyleLanguage::Css => Some("css"),
+            StyleLanguage::Sass => Some("sass"),
+            // Default SCSS: omit lang (matches author surface default).
+            StyleLanguage::Scss => None,
+        };
+        parts.push(emit_tagged_block("style", lang_attr, style_body, settings));
+    }
+
+    parts.push(emit_script_block(&parsed.client, client, settings));
+
+    if let (Some(server_body), Some(server_block)) = (server, parsed.server.as_ref()) {
+        parts.push(emit_script_block(server_block, server_body, settings));
+    }
+
+    let mut out = parts.join(nl);
+    if settings.insert_final_newline {
+        if !out.ends_with(nl) {
+            out.push_str(nl);
+        }
+    } else {
+        while out.ends_with('\n') || out.ends_with('\r') {
+            out.pop();
+        }
+    }
+    out
+}
+
+fn emit_data_block(name: &str, block: &DataBlock, settings: &EditorSettings) -> String {
+    let nl = settings.newline();
+    let attrs = block.attrs.trim();
+    let lang = block.lang.as_deref().filter(|s| !s.is_empty());
+
+    let mut open = format!("<{name}");
+    if !attrs.is_empty() {
+        open.push(' ');
+        open.push_str(attrs);
+    }
+    if let Some(lang) = lang {
+        // Avoid duplicating lang if already in attrs.
+        if !attrs.to_ascii_lowercase().contains("lang=") {
+            open.push_str(&format!(" lang=\"{lang}\""));
+        }
+    }
+
+    let body = block.content.trim();
+    if body.is_empty() {
+        // Self-closing when original had no body (attrs-only sugar).
+        if attrs.contains('/') || block.content.is_empty() {
+            // Prefer explicit close when we only have empty content from a pair of tags;
+            // self-close when attrs look like opening-tag sugar without body.
+            if block.content.is_empty() && !attrs.is_empty() {
+                return format!("{open} />");
+            }
+        }
+        return format!("{open}>{nl}</{name}>");
+    }
+
+    let indented = indent_block(body, settings);
+    format!("{open}>{nl}{indented}</{name}>")
+}
+
+fn emit_tagged_block(
+    name: &str,
+    lang: Option<&str>,
+    body: &str,
+    settings: &EditorSettings,
+) -> String {
+    let nl = settings.newline();
+    let open = match lang {
+        Some(lang) => format!("<{name} lang=\"{lang}\">"),
+        None => format!("<{name}>"),
+    };
+    let body = ensure_trailing_nl(body, nl);
+    format!("{open}{nl}{body}</{name}>")
+}
+
+fn emit_script_block(block: &ScriptBlock, body: &str, settings: &EditorSettings) -> String {
+    let nl = settings.newline();
+    let role = match block.kind {
+        vmz_compiler::ScriptKind::Client => "client",
+        vmz_compiler::ScriptKind::Server => "server",
+    };
+    let open = match block.lang {
+        ScriptLanguage::Ts => format!("<script {role}>"),
+        other => format!("<script {role} lang=\"{}\">", other.as_str()),
+    };
+    let body = ensure_trailing_nl(body, nl);
+    format!("{open}{nl}{body}</script>")
+}
+
+fn indent_block(content: &str, settings: &EditorSettings) -> String {
+    let nl = settings.newline();
+    let unit = settings.indent_unit();
+    let mut out = String::new();
+    for line in content.lines() {
+        let line = if settings.trim_trailing_whitespace {
+            line.trim_end()
+        } else {
+            line
+        };
+        if line.is_empty() {
+            out.push_str(nl);
+        } else {
+            out.push_str(&unit);
+            out.push_str(line);
+            out.push_str(nl);
+        }
+    }
+    out
+}
+
+fn ensure_trailing_nl(body: &str, nl: &str) -> String {
+    let mut body = body.to_string();
+    if body.is_empty() {
+        return body;
+    }
+    // Normalize internal newlines to the EditorConfig ending first.
+    body = body.replace("\r\n", "\n").replace('\r', "\n");
+    if nl != "\n" {
+        body = body.replace('\n', nl);
+    }
+    if !body.ends_with(nl) {
+        body.push_str(nl);
+    }
+    body
+}
---
+++ packages/compilers/vmz-formatter/src/script.rs
@@ -0,0 +1,55 @@
+//! Format `<script>` bodies with oxc_formatter (TS) or envelope-only (other langs).
+
+use oxc_allocator::Allocator;
+use oxc_span::SourceType;
+use vmz_compiler::{ScriptBlock, ScriptLanguage};
+
+use crate::editorconfig::EditorSettings;
+
+/// Format one script block. Non-TS languages keep body text (envelope trim only).
+pub fn format_script_block(block: &ScriptBlock, settings: &EditorSettings) -> Result<String, String> {
+    match block.lang {
+        ScriptLanguage::Ts => format_ts(&block.content, settings),
+        ScriptLanguage::Rust | ScriptLanguage::Python | ScriptLanguage::Java => {
+            Ok(envelope_only(&block.content, settings))
+        }
+    }
+}
+
+fn format_ts(source: &str, settings: &EditorSettings) -> Result<String, String> {
+    let allocator = Allocator::new();
+    let options = settings.js_options();
+    let formatted = oxc_formatter::format(&allocator, source, SourceType::ts(), options)
+        .map_err(|d| d.to_string())?;
+    let code = formatted.print().map_err(|e| e.to_string())?.into_code();
+    Ok(normalize_body(&code, settings))
+}
+
+fn envelope_only(source: &str, settings: &EditorSettings) -> String {
+    normalize_body(source, settings)
+}
+
+fn normalize_body(source: &str, settings: &EditorSettings) -> String {
+    let nl = settings.newline();
+    let mut lines: Vec<String> = source
+        .lines()
+        .map(|l| {
+            if settings.trim_trailing_whitespace {
+                l.trim_end().to_string()
+            } else {
+                l.to_string()
+            }
+        })
+        .collect();
+    while lines.last().is_some_and(|l| l.is_empty()) {
+        lines.pop();
+    }
+    let mut out = lines.join(nl);
+    if settings.insert_final_newline || !out.is_empty() {
+        // Script body inside tags always ends with newline before `</script>`.
+        if !out.is_empty() {
+            out.push_str(nl);
+        }
+    }
+    out
+}
